03484936baff71f03318c20f1440f98c9696fff5
CubeSpawn <cubespawn@gmail.com>
2025-07-05 14:22:46 -0500

    Add static mission tree site

---
+++ bundle.js
@@ -0,0 +1,2 @@
+
+document.getElementById('root').innerHTML = '<h2 style="text-align:center;margin-top:40px;">Mission Tree App Placeholder</h2><p style="text-align:center;">The real React app will be bundled here.</p>';
